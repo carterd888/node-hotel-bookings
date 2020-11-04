218d8386bd0077a075c9d03e5f3b0b12333f18b3
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-11-04 13:39:42 +0000

    completed level 2

--- server.js
+++ server.js
@@ -2,19 +2,69 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { allowedNodeEnvironmentFlags } = require("process");
+const { response } = require("express");
 
 app.get("/", function(request, response){
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
+// TODO add your routes and helper functions here:
+
+//read all bookings
+app.get("/bookings", function(request, response) {
+  response.json(bookings);
+})
+
+//read booking by ID
+app.get("/bookings/:id", (request, response) => {
+  const { id } = request.params;
+  const foundBooking = bookings.find((booking) => booking.id == id);
+if(foundBooking) {
+  response.json(foundBooking);
+} response.sendStatus(404);
+});
 
 
-// TODO add your routes and helper functions here
+//deletes booking by ID
+app.delete("/bookings/:id", function(request, response) {
+  const { id } = request.params;
+  const foundBooking = bookings.find((booking) => booking.id == id);
+  if(foundBooking) {
+    bookings.forEach(function(booking, index) {
+      if (booking.id == id) {
+        bookings.splice(index, 1);
+      } 
+    });
+    response.json({ success: true });
+  } response.sendStatus(404);
+});
+
+
+//creates booking
+app.post("/bookings", function(request, response) {
+  let booking = request.body;
+if (
+  booking.id &&
+  booking.roomId &&
+  booking.title &&
+  booking.firstName &&
+  booking.surname &&
+  booking.email &&
+  booking.checkInDate &&
+  booking.checkOutDate
+) {
+  bookings.push(booking);
+  response.json({ success: true }); 
+} response.sendStatus(400);
+});
 
 
 const listener = app.listen(process.env.PORT, function() {
